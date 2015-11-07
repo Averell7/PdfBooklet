a4d2129ab8c2d639001292813bdf7d93917b6400
Averell7 <dy1977@orange.fr>
2015-11-07 19:21:40 +0100

    tests

--- test1.py
+++ test1.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python
 
 
-try :
-    from gi.repository import Gtk
-except :
-    print ("Gtk not accepted")    
-    try :
-        from gi.repository import GTK
-    except :
-        print ("GTK not accepted")
-        from gi.repository import gtk
-            
-
-                    
-
+#from gi.repository import Gtk
 from gi.repository import Poppler
