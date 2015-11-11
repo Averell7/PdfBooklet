f0215206fe4ef724c7a0a3a4d6a9130e18314556
Averell7 <dy1977@orange.fr>
2015-11-11 03:27:31 +0100

    test3

--- test1.py
+++ test1.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 
+f1 = open("toto", "w")
+try :
+    import gi
+except :    
+    f1.write("echec gi")
+    
+try :
+    from gi import repository
+except :    
+    f1.write("echec repository")
+    
 
-import pgi
-pgi.install_as_gi()
 
 
 #from gi.repository import Gtk
