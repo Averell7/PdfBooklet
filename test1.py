f673965ba3447d80c989465844f6df5717259659
Averell7 <dy1977@orange.fr>
2015-11-11 04:11:57 +0100

    test8

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import os, glob
 
 
 f1 = open("log.txt", "w")
@@ -13,7 +13,20 @@ try :
 except :    
     f1.write("\nechec repository")
     
+
+gifile = "/home/travis/virtualenv/python2.7.9/lib/python2.7/site-packages/gi/repository"
+if os.path.isdir(gifile) :
+    f1.write("\ngifile exists")
+    temp = glob.glob(gifile +  "*.*")
+    for data in temp :
+        f1.write(repr(data) + "\n")
+
+
+
+
 f1.close()
+
+
 
 
 #from gi.repository import Gtk
