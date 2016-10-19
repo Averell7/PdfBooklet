8e271a61f028b09a8ad5d5090b2b4f4a1449af23
Averell7 <dy1977@orange.fr>
2016-10-19 19:25:15 +0200

    log update

--- log.py
+++ log.py
@@ -5,7 +5,7 @@ from ftplib import FTP
 ftp = FTP('perso-ftp.orange.fr')     # connect to host, default port
 x = ftp.login('dysmas1956@wanadoo.fr', '4ua7x9x')                     # user anonymous, passwd anonymous@
 print x
-ftp.cwd('transit')               # change into "debian" directory
+ftp.cwd('pdfbooklet')               # change into "debian" directory
 #ftp.retrlines('LIST')           # list directory contents
 #ftp.retrbinary('RETR Archeotes.sqlite', open('Archeotes.sqlite', 'wb').write)
 x = ftp.storbinary('STOR log.txt' , open("log.txt", 'rb'))
