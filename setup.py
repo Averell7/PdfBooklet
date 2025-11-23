9d12777a2c793092ad61f39371ad9c8a2727539e
tomas <tomas.resquin02@gmail.com>
2025-11-23 15:50:08 -0300

    Bump version from 3.1.3 to 3.1.4 in packaging and setup files.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ data_files=[('/usr/share/pdfbooklet/data', glob.glob('pdfbooklet/data/*.*')),
 
 
 setup(name='pdfbooklet',
-      version='3.1.3',
+      version='3.1.4',
       author='GAF Software',
       author_email='Averell7 at sourceforge dot net',
       maintainer='Averell7',
